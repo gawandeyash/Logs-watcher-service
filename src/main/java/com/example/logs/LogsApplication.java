a1cc2ed7b01f8a1622e89af987e2889968235967
gawandeyash <imyashng@gmail.com>
2025-09-03 18:31:08 +0530

    Final log watcher service

--- src/main/java/com/example/logs/LogsApplication.java
+++ src/main/java/com/example/logs/LogsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class LogsApplication {
-
+	// Logs watcher service
 	public static void main(String[] args) {
 		SpringApplication.run(LogsApplication.class, args);
 	}
